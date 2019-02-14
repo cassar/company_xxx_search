bcfebfbf47096fe9b71b4782e582d74d18e8ae50
Luke Cassar <lukecassar7@gmail.com>
2019-02-14 17:36:21 +1000

    move notation processor test output to text file.

--- specs/models/notation_processor_spec.rb
+++ specs/models/notation_processor_spec.rb
@@ -16,11 +16,8 @@ RSpec.describe NotationProcessor do
 
     notation_processor = NotationProcessor.new(queries, classifications)
 
-    answer = ''
-    answer += "Q1: P2 P1 P3\n"
-    answer += "Q2: P1 P3 P2\n"
-    answer += "Q3: P1 P3 P2\n"
+    output = File.new('specs/fixtures/processor_output.txt').read
 
-    expect(notation_processor.to_s).to eq(answer)
+    expect(notation_processor.to_s).to eq(output)
   end
 end
